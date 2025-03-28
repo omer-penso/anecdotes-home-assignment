8834871508a64aa785a573128730ea1422c4c1ce
omer penso <omer.penso@post.runi.ac.il>
2025-03-28 10:46:07 +0300

    Implement get_authenticated_user

--- auth.py
+++ auth.py
@@ -10,6 +10,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 LOGIN_URL = "https://dummyjson.com/auth/login"
+ME_URL = "https://dummyjson.com/auth/me"
 
 def login(username: str, password: str, expires_in_mins: int = 30):
     try:
@@ -33,4 +34,24 @@ def login(username: str, password: str, expires_in_mins: int = 30):
 
     except Exception as e:
         logger.exception("Exception occurred during login for user: %s", username)
+        return None
+
+
+def get_authenticated_user(access_token: str):
+    try:
+        response = requests.get(
+            ME_URL,
+            headers={"Authorization": f"Bearer {access_token}"},
+        )
+
+        if response.status_code == 200:
+            user_data = response.json()
+            logger.info("Fetched authenticated user details successfully.")
+            return user_data
+        else:
+            logger.error("Failed to fetch authenticated user. Status: %s - %s", response.status_code, response.text)
+            return None
+
+    except Exception:
+        logger.exception("Exception occurred while fetching authenticated user.")
         return None
