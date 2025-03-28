0e354a42f5ad08130fe200cf5012fa41ad559b67
omer penso <omer.penso@post.runi.ac.il>
2025-03-28 11:11:57 +0300

    added test_posts_with_comments_structure()

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -1,5 +1,5 @@
 import pytest
-from posts import get_posts
+from posts import get_posts, get_posts_with_comments
 
 def test_get_posts_should_success():
     posts = get_posts()
@@ -11,3 +11,14 @@ def test_get_posts_custom_limit():
     posts = get_posts(limit=10)
     assert posts is not None
     assert len(posts) == 10
+
+def test_posts_with_comments_structure():
+    posts = get_posts_with_comments(limit=5)
+    assert posts is not None
+    for post in posts:
+        assert "id" in post
+        assert "title" in post
+        assert "comments" in post
+        if post["comments"]:
+            assert "body" in post["comments"][0]
+            assert "user" in post["comments"][0]
