6bbbd4a890affd5d48f12f36edc444bfa11b28bb
omer penso <omer.penso@post.runi.ac.il>
2025-03-28 10:51:03 +0300

    added unit tests for get_authenticated_use

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,5 +1,5 @@
 import pytest
-from auth import login
+from auth import login, get_authenticated_user
 
 
 def test_login_should_succeed():
@@ -11,3 +11,18 @@ def test_login_should_succeed():
 def test_login_should_fail():
     result = login("omerp", "omer1234")
     assert result is None
+
+
+def test_get_authenticated_user_should_success():
+    credentials = {"username": "emilys", "password": "emilyspass"}
+    tokens = login(**credentials)
+    assert tokens is not None, "Login failed, no tokens returned"
+
+    user = get_authenticated_user(tokens["accessToken"])
+    assert user is not None, "Failed to fetch authenticated user"
+    assert user["username"] == credentials["username"]
+
+
+def test_get_authenticated_user_invalid_token():
+    user = get_authenticated_user("invalid_token")
+    assert user is None
